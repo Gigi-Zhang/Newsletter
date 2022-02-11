4c0051d79b5508b3b53dc865044ec1d0d7f5c5f3
greatxiaozhe@gmail.com <greatxiaozhe@gmail.com>
2022-02-11 21:36:49 +0800

    Change the http request to mailchimp method

--- app.js
+++ app.js
@@ -1,92 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("request");
-const https = require("https");
-const mailchimp = require("@mailchimp/mailchimp_marketing");
+const client = require("@mailchimp/mailchimp_marketing");
+const request = require("request")
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-mailchimp.setConfig({
+// mailchimp config
+client.setConfig({
   apiKey: process.env.APIKEY,
   server: process.env.SEVER_PREFIX,
 });
 
-//GET INFORMATIONS ABOUT ALL AUDIANCE
-// const run = async () => {
-//   const response = await mailchimp.lists.getAllLists();
-//   console.log(response);
-// };
-//
-// run();
+// allowing app to use body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 
-//GET INFORMATIONS ABOUT SPECEFIC AUDIANCE
-// const getList = async () => {
-//   const response = await mailchimp.lists.getList("ea08ba2f0e");
-//   console.log(response);
-// };
-//
-// getList();
-//
+// allowing app to use express.static to load static files
+app.use(express.static("public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
-//
-app.post("/", async (req, res) => {
-      var firstName = req.body.firstName;
-      var lastName = req.body.lastName;
-      var email = req.body.email;
 
-      const apiKey = process.env.APIKEY;
-      const listId = process.env.LIST_ID;
+app.post("/", function (req, res) {
+  // getting information from the signup page using body-parser
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-      //Member sync comes from add member method in mailchimp
-      const data = await mailchimp.lists.addListMember(process.env.LIST_ID, {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName
-        }
-      });
-
-      const jsonData = JSON.stringify(data);
-
-      const url = process.env.URL;
-      const options = {
-        method: "POST",
-        auth: process.env.OPTIONS_AUTH
+// sending request and creating the data to send to the external server
+  const run = async () => {
+  const response = await client.lists.batchListMembers(process.env.LIST_ID, {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
       }
+    }],
+  });
+  console.log(response.errors[0]);
+  if (response.errors[0] === undefined){
+    res.sendFile(__dirname + "/success.html");
+  }else{
+    res.sendFile(__dirname + "/failure.html");
+  }
+};
+  run();
+ });
 
-      const request = https.request(url, options, function(response) {
-          if (response.statusCode === 200) {
-            res.sendFile(__dirname + "/success.html");
-          } else {
-            res.sendFile(__dirname + "/failure.html");
-          };
 
-          console.log(response);
-          response.on("data", function(data) {
-              if (data != null) {
-                console.log(JSON.parse(data));
-                }
-          });
-        });
+app.post("/failure", function (req, res) {
+  res.redirect("/")
+})
 
-        request.write(jsonData);
-        request.end();
-      });
-
-    app.post("/failure", function(req, res) {
-      res.redirect("/");
-    });
-
-    app.listen(process.env.PORT || 3000, function() {
-      console.log("Server is running on port 3000.")
-    });
+app.listen(3000, function (req, res) {
+  console.log("Server is running on port 3000");
+})
