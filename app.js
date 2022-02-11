be626b85dcb5c67c9eec316a388d207b8752fe04
greatxiaozhe@gmail.com <greatxiaozhe@gmail.com>
2022-02-11 19:40:04 +0800

    Everything is fine but post result, mailchimp keeps resending error page. I assume the problem is uri but can't figure it out

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
-const dotenv = require("dotenv");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,57 +12,81 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mailchimp.setConfig({
+  apiKey: process.env.APIKEY,
+  server: process.env.SEVER_PREFIX,
+});
+
+//GET INFORMATIONS ABOUT ALL AUDIANCE
+// const run = async () => {
+//   const response = await mailchimp.lists.getAllLists();
+//   console.log(response);
+// };
+//
+// run();
+
+//GET INFORMATIONS ABOUT SPECEFIC AUDIANCE
+// const getList = async () => {
+//   const response = await mailchimp.lists.getList("ea08ba2f0e");
+//   console.log(response);
+// };
+//
+// getList();
+//
+
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
+//
+app.post("/", async (req, res) => {
+      var firstName = req.body.firstName;
+      var lastName = req.body.lastName;
+      var email = req.body.email;
 
-app.post("/", function(req, res) {
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var eamil = req.body.email;
+      const apiKey = process.env.APIKEY;
+      const listId = process.env.LIST_ID;
 
-  var data = {
-    members: [{
-      email_address: eamil,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: firstName,
-        LNAME: lastName
+      //Member sync comes from add member method in mailchimp
+      const data = await mailchimp.lists.addListMember(process.env.LIST_ID, {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      });
+
+      const jsonData = JSON.stringify(data);
+
+      const url = process.env.URL;
+      const options = {
+        method: "POST",
+        auth: process.env.OPTIONS_AUTH
       }
-    }]
 
-  };
+      const request = https.request(url, options, function(response) {
+          if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+          } else {
+            res.sendFile(__dirname + "/failure.html");
+          };
 
-  const jsonData = JSON.stringify(data);
+          console.log(response);
+          response.on("data", function(data) {
+              if (data != null) {
+                console.log(JSON.parse(data));
+                }
+          });
+        });
 
-  const apiKey = process.env.APIKEY;
-  const listId = process.env.LIST_ID;
-  const url = process.env.URL;
-  const options = {
-    method: "POST",
-    auth: process.env.OPTIONS_AUTH
-  }
+        request.write(jsonData);
+        request.end();
+      });
 
-  const request = https.request(url, options, function(response) {
-    if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else {
-      res.sendFile(__dirname + "/failure.html");
-    }
-    response.on("data", function(data) {
-      console.log(JSON.parse(data));
+    app.post("/failure", function(req, res) {
+      res.redirect("/");
     });
 
-  });
-
-  request.write(jsonData);
-  request.end();
-});
-
-app.post("/failure",function(req,res){
-  res.redirect("/");
-})
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port 3000.")
-})
+    app.listen(process.env.PORT || 3000, function() {
+      console.log("Server is running on port 3000.")
+    });
